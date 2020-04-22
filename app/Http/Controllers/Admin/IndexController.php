3ad7287e38a8b2369b833037c7d0ec35f7e033c3
13819198336 <10615l>
2020-04-23 00:29:29 +0800

    提交两个忘记的控制器

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    //后台首页先
+    public function index()
+    {
+        return view('admin.index.index');
+    }
+
+    //欢迎页面
+    public function welcome()
+    {
+        return view('admin.index.welcome');
+    }
+}
---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+
+class LoginController extends Controller
+{
+    //登录显示
+    public function index()
+    {
+        //判断用户是否已经登陆过
+        if(auth()->check()){
+            redirect(route('admin.index'));
+        }
+        return view('admin.login.login');
+    }
+
+    //登录
+    public function login(Request $request)
+    {
+        //表单认证
+       $post =  $this->validate($request,[
+            'username' => 'required',
+            'password' => 'required',
+        ],[
+           'username.required'=>'账号都不写，想飞吗',
+           'password.required'=>'密码都不写，想飞吗',
+       ]);
+
+       //登录
+        $bool = auth()->guard('web')->attempt($post);
+        if($bool){
+            //auth()->user() 返回当前登录的用户模型对象 存储在session中
+            //laravel默认session时存储在文件中 优化到memcached redis
+//            $model = auth()->user();
+//            dd($model->toArray());
+            return redirect(route('admin.index'));
+        }
+        //withErrors 把信息写入到验证错误提示中 特殊的session laravel中叫 闪存
+        //闪存 从设置好之后 只能在第一个http请求中获取到数据 以后就没有了
+        return redirect(route('admin.login'))->withErrors(['error'=>'登录失败']);
+
+    }
+}
